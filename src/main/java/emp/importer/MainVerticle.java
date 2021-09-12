3c084ea8622657f8df5a4c6c2c9c18c447d40e72
Karthik <karthik253bc@gmail.com>
2021-09-12 14:38:22 +0530

    MySQL connection

--- src/main/java/emp/importer/MainVerticle.java
+++ src/main/java/emp/importer/MainVerticle.java
@@ -7,6 +7,11 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -15,6 +20,33 @@ public class MainVerticle extends AbstractVerticle {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
+
+//    DB connection configuration
+    MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+      .setPort(3306)
+      .setHost("localhost")
+      .setDatabase("dev")
+      .setUser("root")
+      .setPassword("root");
+// Pool options
+    PoolOptions poolOptions = new PoolOptions()
+      .setMaxSize(20);
+// Create the client pool
+    MySQLPool dbCient = MySQLPool.pool(vertx, connectOptions, poolOptions);
+// A simple query
+    dbCient
+      .query("select * from Employee")
+      .execute(ar -> {
+        if (ar.succeeded()) {
+          RowSet<Row> result = ar.result();
+          System.out.println("Got " + result.size() + " rows ");
+        } else {
+          System.out.println("Failure: " + ar.cause().getMessage());
+        }
+        // Now close the pool
+        dbCient.close();
+      });
+
 
     router.route(HttpMethod.POST, "/v1/employee/bulk").handler(EmployeeService::postEmployeeBulkRoute);
     router.route(HttpMethod.GET, "/ping").handler(EmployeeService::getPingRoute);
