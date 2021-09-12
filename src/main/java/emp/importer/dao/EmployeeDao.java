0f21fd3a6935ba8942956662730bdf937f4a41ca
Karthik <karthik253bc@gmail.com>
2021-09-12 15:30:58 +0530

    insert EmployeesDAO

---
+++ src/main/java/emp/importer/dao/EmployeeDao.java
@@ -0,0 +1,53 @@
+package emp.importer.dao;
+
+import emp.importer.payload.Employee;
+import io.vertx.core.Future;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.Tuple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDao {
+
+  public static void insertEmployees(List<Employee> empList, MySQLPool dbClient) {
+    List<Tuple> batch = new ArrayList<>();
+    for (Employee emp : empList) {
+      batch.add(Tuple.of(emp.getEmployeeId(), emp.getFirstName(), emp.getLastName(), emp.getEmail(), emp.getLocation()));
+    }
+    dbClient
+      .preparedQuery("INSERT INTO Employee (EmployeeId, FirstName, LastName, Email, Location) VALUES (?, ?, ?, ?, ?)")
+      .executeBatch(batch, res -> {
+        if (res.succeeded()) {
+          RowSet<Row> rows = res.result();
+        } else {
+          System.out.println("Batch failed " + res.cause());
+        }
+      });
+  }
+
+  public static Future<SqlConnection> insertEmployeesAsync(List<Employee> empList, MySQLPool dbClient) {
+    List<Tuple> batch = new ArrayList<>();
+    for (Employee emp : empList) {
+      batch.add(Tuple.of(emp.getEmployeeId(), emp.getFirstName(), emp.getLastName(), emp.getEmail(), emp.getLocation()));
+    }
+
+    return dbClient.getConnection()
+      .onSuccess(conn -> {
+        conn.begin()
+          .compose(tx -> conn
+            .preparedQuery("INSERT INTO Employee (EmployeeId, FirstName, LastName, Email, Location) VALUES (?, ?, ?, ?, ?)")
+            .executeBatch(batch)
+            .compose(res3 -> tx.commit()))
+          .eventually(v -> conn.close());
+//          .onSuccess(v -> System.out.println("Transaction succeeded"))
+//          .onFailure(err -> System.out.println("Transaction failed: " + err.getMessage()));
+      });
+  }
+
+
+}
